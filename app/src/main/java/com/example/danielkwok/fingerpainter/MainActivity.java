e61da55c1975dd81a002ea4990254a5abf8fee8c
unknown <danielzhengxian21@gmail.com>
2018-10-09 16:41:06 +0800

    implemented fingerPainterView

--- app/src/main/java/com/example/danielkwok/fingerpainter/MainActivity.java
+++ app/src/main/java/com/example/danielkwok/fingerpainter/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.danielkwok.fingerpainter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.danielkwok.fingerpainter.Views.FingerPainterView;
+
 public class MainActivity extends AppCompatActivity {
+
+    FingerPainterView myFingerPainterView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myFingerPainterView = findViewById(R.id.myFingerPainterViewId);
+
+
     }
 }
