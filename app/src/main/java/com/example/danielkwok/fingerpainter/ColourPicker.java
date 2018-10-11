b74a4f4d77506bf64417c976e8f0ad946e914e1e
Daniel Kwok <danielzhengxian21@gmail.com>
2018-10-11 15:33:14 +0800

    tiles with array working

--- app/src/main/java/com/example/danielkwok/fingerpainter/ColourPicker.java
+++ app/src/main/java/com/example/danielkwok/fingerpainter/ColourPicker.java
@@ -1,48 +1,86 @@
 package com.example.danielkwok.fingerpainter;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.support.v4.widget.ImageViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.example.danielkwok.fingerpainter.Utils.Utils;
 import com.skydoves.colorpickerview.ColorPickerView;
 import com.skydoves.colorpickerview.listeners.ColorListener;
+
+import java.util.ArrayList;
 
 public class ColourPicker extends AppCompatActivity {
     private static final String TAG = "ColourPicker";
     private ColorPickerView colorPickerView;
     private int currentColour = Color.BLACK;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        int[] pickedColours ={
+                getResources().getColor(R.color.black),
+                getResources().getColor(R.color.white),
+                getResources().getColor(R.color.red),
+                getResources().getColor(R.color.orange),
+                getResources().getColor(R.color.yellow),
+                getResources().getColor(R.color.green),
+                getResources().getColor(R.color.blue),
+                getResources().getColor(R.color.darkblue),
+                getResources().getColor(R.color.purple),
+                getResources().getColor(R.color.white),
+                getResources().getColor(R.color.white),
+                getResources().getColor(R.color.white)
+        };
+
+        int[] pickedColourTiles = {
+                R.id.colour_picker_color1_iv,
+                R.id.colour_picker_color2_iv,
+                R.id.colour_picker_color3_iv,
+                R.id.colour_picker_color4_iv,
+                R.id.colour_picker_color5_iv,
+                R.id.colour_picker_color6_iv,
+                R.id.colour_picker_color7_iv,
+                R.id.colour_picker_color8_iv,
+                R.id.colour_picker_color9_iv,
+                R.id.colour_picker_color10_iv,
+                R.id.colour_picker_color11_iv,
+                R.id.colour_picker_color12_iv,
+        };
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colour_picker);
+
+        //set tile colours
+        for(int i=0; i<pickedColourTiles.length; i++){
+            ImageView tile = findViewById(pickedColourTiles[i]);
+            tile.setColorFilter(pickedColours[i]);
+        }
 
         colorPickerView = findViewById(R.id.colorPickerView);
         colorPickerView.setColorListener(new ColorListener() {
             @Override
             public void onColorSelected(int color, boolean fromUser) {
                 currentColour = color;
-                Utils.printToast(getApplicationContext(), Integer.toString(currentColour));
+
             }
         });
+
     }
 
-    public void pickColour(View v){
+    public void pickColourFromWheel(){
+
+    }
+
+    public void pickColourFromTile(View v){
         currentColour = ImageViewCompat.getImageTintList((ImageView)v).getDefaultColor();
-        //Utils.printToast(getApplicationContext(), Integer.toString(currentColour));
         Log.d(TAG, "curentColour: "+currentColour);
         Intent intent = new Intent();
         intent.putExtra("pickedColour", currentColour);
